41efc5caef717a405ce0b127017a4fb6d495e6e2
kentxxq <805429509@qq.com>
2024-07-03 17:08:11 +0800

    清理过期消息

---
+++ pusher.webapi/Common/DataOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace pusher.webapi.Common;
+
+public class DataOptions
+{
+    public const string Data = "Data";
+
+    /// <summary>
+    ///     清理超过30天的消息记录
+    /// </summary>
+    public int MessageRetentionPeriod { get; set; } = 30;
+}
--- pusher.webapi/Jobs/CleanUselessInfo.cs
+++ pusher.webapi/Jobs/CleanUselessInfo.cs
@@ -1,3 +1,4 @@
+using pusher.webapi.Common;
 using pusher.webapi.Models.DB;
 using pusher.webapi.Service;
 using pusher.webapi.Service.Database;
@@ -10,7 +11,10 @@ namespace pusher.webapi.Jobs;
 /// </summary>
 public class CleanUselessInfo : IJob
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger<CleanUselessInfo> _logger;
+    private readonly Repository<ChannelMessageHistory> _repChannelMessageHistory;
+    private readonly Repository<Message> _repMessage;
     private readonly Repository<User> _repUser;
     private readonly UserService _userService;
 
@@ -18,11 +22,16 @@ public class CleanUselessInfo : IJob
     ///     依赖注入
     /// </summary>
     /// <param name="logger"></param>
-    public CleanUselessInfo(ILogger<CleanUselessInfo> logger, UserService userService, Repository<User> repUser)
+    public CleanUselessInfo(ILogger<CleanUselessInfo> logger, UserService userService, Repository<User> repUser,
+        IConfiguration configuration, Repository<Message> repMessage,
+        Repository<ChannelMessageHistory> repChannelMessageHistory)
     {
         _logger = logger;
         _userService = userService;
         _repUser = repUser;
+        _configuration = configuration;
+        _repMessage = repMessage;
+        _repChannelMessageHistory = repChannelMessageHistory;
     }
 
     /// <summary>
@@ -34,6 +43,7 @@ public class CleanUselessInfo : IJob
     {
         await DeleteUselessUser();
         await DeleteTestUserData();
+        await DeleteExpiredMessage();
     }
 
     /// <summary>
@@ -58,5 +68,21 @@ public class CleanUselessInfo : IJob
         var user = await _repUser.GetFirstAsync(u => u.Username == "test");
         await _userService.DeleteUserData(user.Id);
         await _userService.InitUserData(user.Id);
+        _logger.LogWarning("已删除test用户的相关数据");
+    }
+
+    /// <summary>
+    ///     清理过期的消息
+    /// </summary>
+    private async Task DeleteExpiredMessage()
+    {
+        var messageRetentionPeriod =
+            _configuration.GetSection(DataOptions.Data).Get<DataOptions>()!.MessageRetentionPeriod;
+        var expiredMessageId =
+            await _repMessage.GetListAsync(m => (DateTime.Now - m.RecordTime).Days > messageRetentionPeriod);
+
+        await _repMessage.DeleteAsync(m => expiredMessageId.Select(e => e.Id).Contains(m.Id));
+        await _repChannelMessageHistory.DeleteAsync(c => expiredMessageId.Select(e => e.Id).Contains(c.MessageId));
+        _logger.LogWarning($"已清理超过{messageRetentionPeriod}天的消息记录");
     }
 }
