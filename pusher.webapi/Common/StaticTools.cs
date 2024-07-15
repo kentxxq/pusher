906b905d8384a8fa2ec4d49ed5aa2b619224fdb0
kentxxq <805429509@qq.com>
2024-07-15 15:22:52 +0800

    同步日志用法

--- pusher.webapi/Common/StaticTools.cs
+++ pusher.webapi/Common/StaticTools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.Sockets;
 using SqlSugar;
 
 namespace pusher.webapi.Common;
@@ -26,5 +27,27 @@ public static class StaticTools
         {
             Credentials = new NetworkCredential(username, password)
         };
+    }
+
+    /// <summary>
+    ///     获取本机ipv4内网ip<br />
+    ///     如果网络不可用返回127.0.0.1<br />
+    ///     如果之前网络可用，可能会返回之前保留下来的ip地址
+    /// </summary>
+    /// <returns></returns>
+    public static IPAddress GetLocalIP()
+    {
+        try
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("223.5.5.5", 53);
+            var endPoint = socket.LocalEndPoint as IPEndPoint;
+            endPoint ??= new IPEndPoint(IPAddress.Loopback, 0);
+            return endPoint.Address;
+        }
+        catch (Exception)
+        {
+            return IPAddress.Loopback;
+        }
     }
 }
--- pusher.webapi/Extensions/LogExtensions.cs
+++ pusher.webapi/Extensions/LogExtensions.cs
@@ -7,6 +7,9 @@ namespace pusher.webapi.Extensions;
 
 public static class LogExtensions
 {
+    private const string DefaultLogTemplate =
+        "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{MachineIP}|{MachineName}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}";
+
     private static LoggerConfiguration AddCommonConfig(this LoggerConfiguration loggerConfiguration)
     {
         return loggerConfiguration
@@ -14,6 +17,8 @@ public static class LogExtensions
             .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
             .MinimumLevel.Override("System", LogEventLevel.Warning)
             .Enrich.WithProperty("AppName", ThisAssembly.Project.AssemblyName)
+            .Enrich.WithMachineName()
+            .Enrich.WithProperty("MachineIP", StaticTools.GetLocalIP().ToString())
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("SourceContext"),
                 enrichmentConfig => enrichmentConfig.WithProperty("SourceContext", "SourceContext"))
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("ThreadName"),
@@ -32,10 +37,14 @@ public static class LogExtensions
                 formatter: MyJsonFormatter.Formatter,
                 rollingInterval: RollingInterval.Day,
                 retainedFileCountLimit: 1))
+            .WriteTo.Async(l => l.File(
+                $"stdout-{ThisAssembly.Project.AssemblyName}-.log",
+                outputTemplate: DefaultLogTemplate,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 1))
             .WriteTo.Async(l =>
                 l.Console(
-                    outputTemplate:
-                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}",
+                    outputTemplate: DefaultLogTemplate,
                     theme: AnsiConsoleTheme.Code));
     }
 
@@ -50,10 +59,14 @@ public static class LogExtensions
                 rollingInterval: RollingInterval.Day,
                 retainedFileCountLimit: configuration.GetValue("KLog:File:RetainedFileCountLimit", 1))
             )
+            .WriteTo.Async(l => l.File(
+                $"stdout-{ThisAssembly.Project.AssemblyName}-.log",
+                outputTemplate: DefaultLogTemplate,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 1))
             .WriteTo.Async(l =>
                 l.Console(
-                    outputTemplate: configuration["KLog:Console:OutputTemplate"] ??
-                                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}",
+                    outputTemplate: configuration["KLog:Console:OutputTemplate"] ?? DefaultLogTemplate,
                     theme: AnsiConsoleTheme.Code)
             );
     }
