aa68ee26dfe1a628b46c852e6e99a9665b213b52
kentxxq <805429509@qq.com>
2024-10-08 17:14:31 +0800

    修正Qmsg发送失败的问题

--- pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
+++ pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
@@ -1,3 +1,6 @@
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
 using pusher.webapi.Enums;
 
 namespace pusher.webapi.Service.ChannelHandler.Qmsg;
@@ -20,9 +23,21 @@ public class QmsgChannelHandlerHttp : ChannelHandlerHttpBase
     /// <inheritdoc />
     public override async Task<HandlerResult> HandleText(string url, string content, string proxy)
     {
-        var data = new QmsgText() { Msg = content };
+        var data = new QmsgText { Msg = content };
         var httpClient = GetHttpClient(proxy);
-        var httpResponseMessage = await httpClient.PostAsJsonAsync(url, data);
+
+
+        // PostAsJsonAsync会报错
+        // https://www.cnblogs.com/yexiaoyanzi/p/16309697.html
+        // https://stackoverflow.com/questions/59590012/net-httpclient-accept-partial-response-when-response-header-has-an-incorrect
+        // var httpResponseMessage = await httpClient.PostAsJsonAsync(url, data);
+        using var req = new HttpRequestMessage(HttpMethod.Post, url);
+
+        // 这里用jsonContent也会报错,原因未知...
+        // req.Content = JsonContent.Create(data);
+        req.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, MediaTypeNames.Application.Json);
+        var httpResponseMessage = await httpClient.SendAsync(req);
+
         var result = await httpResponseMessage.Content.ReadFromJsonAsync<QmsgResponse>();
         return result?.Success == true
             ? new HandlerResult { IsSuccess = true }
