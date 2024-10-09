54fcecf2d8b322a0bde30ff9c9550a7deb5fc81a
kentxxq <805429509@qq.com>
2024-10-09 17:02:10 +0800

    学习了Content-Length和Transfer-Encoding , 加上了注释

--- pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
+++ pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
@@ -26,17 +26,22 @@ public class QmsgChannelHandlerHttp : ChannelHandlerHttpBase
         var data = new QmsgText { Msg = content };
         var httpClient = GetHttpClient(proxy);
 
+        // 背景: Qmsg启用了http2连接, 而http2不支持chunked编码("Transfer-Encoding": "chunked"), 所以需要设置Content-Length.
+        // https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Transfer-Encoding
 
-        // PostAsJsonAsync会报错
-        // https://www.cnblogs.com/yexiaoyanzi/p/16309697.html
-        // https://stackoverflow.com/questions/59590012/net-httpclient-accept-partial-response-when-response-header-has-an-incorrect
-        // var httpResponseMessage = await httpClient.PostAsJsonAsync(url, data);
+        // 使用 using var req = new HttpRequestMessage(HttpMethod.Post, url); 报错
+        //   System.Net.Http.HttpRequestException: An error occurred while sending the request.
+        //   System.Net.Http.HttpI0Exception: The response ended prematurely. (ResponseEnded)
+        // 这是因为PostAsJsonAsync使用了jsonContent.Create 这是一个stream,所以不会计算长度,也就无法获得Content-Length
+        // 即使设置req.Headers.TransferEncodingChunked = false,因为没有Content-Length, 导致TransferEncodingChunked也没有生效
+
+        // 这里提前序列化成string, 采用StringContent就会带上 Content-Length, 请求正常
         using var req = new HttpRequestMessage(HttpMethod.Post, url);
-
-        // 这里用jsonContent也会报错,原因未知...
-        // req.Content = JsonContent.Create(data);
         req.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, MediaTypeNames.Application.Json);
         var httpResponseMessage = await httpClient.SendAsync(req);
+
+        req.Content = JsonContent.Create(data);
+        req.Headers.TransferEncodingChunked = false;
 
         var result = await httpResponseMessage.Content.ReadFromJsonAsync<QmsgResponse>();
         return result?.Success == true
