03f11a91a228a7631399a1b89800778960561c4d
kentxxq <805429509@qq.com>
2024-10-09 17:33:43 +0800

    JsonContent通过LoadIntoBufferAsync也可以使用Content-Length形式发送数据

--- pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
+++ pusher.webapi/Service/ChannelHandler/Qmsg/QmsgChannelHandlerHttp.cs
@@ -29,17 +29,27 @@ public class QmsgChannelHandlerHttp : ChannelHandlerHttpBase
         // 背景: Qmsg启用了http2连接, 而http2不支持chunked编码("Transfer-Encoding": "chunked"), 所以需要设置Content-Length.
         // https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/Transfer-Encoding
 
-        // 使用 using var req = new HttpRequestMessage(HttpMethod.Post, url); 报错
+        // 使用 var httpResponseMessage = await httpClient.PostAsJsonAsync(url,data) 报错
         //   System.Net.Http.HttpRequestException: An error occurred while sending the request.
         //   System.Net.Http.HttpI0Exception: The response ended prematurely. (ResponseEnded)
-        // 这是因为PostAsJsonAsync使用了jsonContent.Create 这是一个stream,所以不会计算长度,也就无法获得Content-Length
-        // 即使设置req.Headers.TransferEncodingChunked = false,因为没有Content-Length, 导致TransferEncodingChunked也没有生效
+        // 这是因为 PostAsJsonAsync 默认使用JsonContent返回一个stream,所以不会计算长度,也就无法获得Content-Length
 
-        // 这里提前序列化成string, 采用StringContent就会带上 Content-Length, 请求正常
+
+        // 1. 先创建HttpRequestMessage
         using var req = new HttpRequestMessage(HttpMethod.Post, url);
-        req.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, MediaTypeNames.Application.Json);
-        var httpResponseMessage = await httpClient.SendAsync(req);
 
+        // 2 设置content,这会影响到Content-Length
+        // 采用 JsonContent, 即使设置req.Headers.TransferEncodingChunked = false,因为没有Content-Length, 导致TransferEncodingChunked也没有生效
+        // 根据这个issues https://github.com/dotnet/runtime/issues/30283 ,可以通过LoadIntoBufferAsync把内容加载到memory中,这样JsonContent也能使用Content-Length发送
+        // req.Content = JsonContent.Create(data);
+        // await req.Content.LoadIntoBufferAsync();
+
+        // 2 设置content,这会影响到Content-Length
+        // 采用 StringContent. 手动序列化成string, 就会带上 Content-Length
+        req.Content = new StringContent(JsonSerializer.Serialize(data), Encoding.UTF8, MediaTypeNames.Application.Json);
+
+        // 3 发送请求,拿到结果
+        var httpResponseMessage = await httpClient.SendAsync(req);
         var result = await httpResponseMessage.Content.ReadFromJsonAsync<QmsgResponse>();
         return result?.Success == true
             ? new HandlerResult { IsSuccess = true }
